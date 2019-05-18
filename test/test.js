316a3de018485e82aca7640a4888ae865a3b5d1f
Coenie Pretorius <csp.pretorius@gmail.com>
2019-05-18 13:57:38 +0200

    Added create, list and delete endpoints and added tests.

--- test/test.js
+++ test/test.js
@@ -9,22 +9,109 @@ mocha.describe("To Do List", function() {
     after(function () {
         app.close();
     });
-    mocha.describe("Test /", function() {
-        it("GET returns 200", function(done) {
-            request(app).get("/").expect(200, done);
+
+    mocha.describe("Read /", function() {
+        const test_url = "/";
+        const expected_ct = "text/html; charset=utf-8";
+
+        it("Read returns 200", function(done) {
+            request(app).get(test_url).expect(200, done);
         });
-        it("GET returns data", function(done) {
-            request(app).get("/todo").expect(function(res) {
+        it("Read returns HTML", function(done) {
+            request(app).get(test_url)
+                .expect('Content-Type', expected_ct, done);
+        });
+        it("Read returns data", function(done) {
+            request(app).get(test_url).expect(function(res) {
                 let cl = parseInt(res.headers['content-length']);
                 if (!(cl >= 0)) throw new Error("Body is empty");
             }).end(done);
         });
-        it("GET returns HTML", function(done) {
-            request(app).get("/todo").expect(function(res) {
-                let ct = res.headers['content-type'];
-                let is_html = ct.includes("text/html");
-                if (!is_html) throw new Error("No HTML returned.");
-            }).end(done);
+    });
+    mocha.describe("List /todo/", function() {
+        const test_url = "/todo/";
+        const expected_ct = "application/json; charset=utf-8";
+
+        it("List returns 200", function (done) {
+            request(app).get(test_url).expect(200, done);
+        });
+        it("List returns JSON", function(done) {
+            request(app).get(test_url)
+                .expect('Content-Type', expected_ct, done);
+        });
+     });
+     mocha.describe("Create /todo/", function() {
+         const test_url = "/todo/";
+         const expected_ct = "application/json; charset=utf-8";
+         const test_data_bad_field_name = {foo: ""};
+         const test_data_bad_field_name_response = {
+             error: "Field 'todo' required."
+         };
+         const test_data_empty_field = {todo: ""};
+         const test_data_empty_field_response = {
+             error: "Field 'todo' cannot be empty."
+         };
+
+         const test_str = "foo";
+         const test_data_good = {todo: test_str};
+         const test_data_good_expect = {id: 0, todo: test_str};
+         it("Create requires todo field", function (done) {
+             request(app).post(test_url)
+                 .send(test_data_bad_field_name)
+                 .expect('Content-Type', expected_ct)
+                 .expect(400, test_data_bad_field_name_response, done);
+         });
+         it("Create requires non-empty todo field", function (done) {
+            request(app).post(test_url)
+                .send(test_data_empty_field)
+                .expect('Content-Type', expected_ct)
+                .expect(400, test_data_empty_field_response, done);
+         });
+         it("Create with good data returns 201, data and JSON", function (done) {
+            request(app).post(test_url)
+                .send(test_data_good)
+                .expect('Content-Type', expected_ct)
+                .expect(201, test_data_good_expect, done);
+         });
+         it("Create stores data in list", function(done) {
+            request(app).get(test_url).expect([test_data_good_expect], done);
+         });
+    });
+    mocha.describe("Delete /todo/", function() {
+        const test_url = "/todo/";
+        const expected_ct = "application/json; charset=utf-8";
+        const test_str = "foo";
+        const test_data_good_expect = [{id: 0, todo: test_str}];
+
+        // NOTE(coenie): We expect an item from the previous create tests to
+        // still be in the list.
+        it("Ensure data in list", function(done) {
+            request(app).get(test_url)
+                .expect('Content-Type', expected_ct)
+                .expect(test_data_good_expect, done);
+        });
+        it("Negative IDs not allowed", function(done) {
+            let expected_error = {"error": "Negative IDs not allowed."};
+            request(app).delete("/todo/-1/")
+                .expect('Content-Type', expected_ct)
+                .expect(400, expected_error, done);
+        });
+        it("Only existing IDs allowed.", function(done) {
+            let expected_error = {"error": "ID does not exist."};
+            request(app).delete("/todo/1/")
+                .expect('Content-Type', expected_ct)
+                .expect(400, expected_error, done);
+        });
+        it("Valid delete works as expected.", function(done) {
+            let expected_message = {"message": "Deleted."};
+            request(app).delete("/todo/0/")
+                .expect('Content-Type', expected_ct)
+                .expect(200, expected_message, done);
+        });
+        it("Ensure list is now empty", function(done) {
+            request(app).get(test_url)
+                .expect('Content-Type', expected_ct)
+                .expect([], done);
         });
     });
 });
--- todo.js
+++ todo.js
@@ -8,13 +8,73 @@ let todo_list = [];
 
 app.use(json_parser);
 
-function render_index(req, res) {
+function read_index(req, res) {
     res.status(200).render("index.ejs", {});
+}
+
+function list_todo(req, res) {
+    let output_list = [];
+    todo_list.forEach((value, index) => {
+        output_list.push({id: index, todo: value});
+    });
+    res.status(200).json(output_list);
+}
+
+function create_todo(req, res) {
+    let return_status = 500;
+    let return_data = {error: "Unknown error."};
+
+    let todo_provided = "todo" in req.body;
+    let todo_empty = req.body.todo === "";
+
+    if (!todo_provided) {
+        return_status = 400;
+        return_data.error = "Field 'todo' required.";
+    }
+
+    if (todo_provided && todo_empty) {
+        return_status = 400;
+        return_data.error = "Field 'todo' cannot be empty.";
+    }
+
+    if (todo_provided && !todo_empty) {
+        let idx = todo_list.push(req.body.todo) -1;
+        return_status = 201;
+        return_data = {id: idx, todo: req.body.todo}
+    }
+
+    res.status(return_status).json(return_data);
+}
+
+function delete_todo(req, res) {
+    let return_status = 500;
+    let return_data = {"error": "Unknown error."};
+
+    let request_id = parseInt(req.params.id);
+
+    if (request_id < 0) {
+        return_status = 400;
+        return_data = {"error": "Negative IDs not allowed."};
+    }
+
+    if (request_id > todo_list.length - 1) {
+        return_status = 400;
+        return_data = {"error": "ID does not exist."};
+    }
+    if (request_id >= 0 && request_id <= todo_list.length - 1) {
+        todo_list.splice(request_id, 1);
+        return_status = 200;
+        return_data = {"message": "Deleted."};
+    }
+    res.status(return_status).json(return_data);
 }
 
 // NOTE(coenie): Render the index page which handles the client side
 // interaction.
-app.get("/", render_index);
+app.get("/", read_index);
+app.get("/todo/", list_todo);
+app.post("/todo/", create_todo);
+app.delete("/todo/:id", delete_todo);
 
 let status_str = `My To Do list running on port ${port}!`;
 module.exports = app.listen(port, () => console.log(status_str));
